dc31b26c8ff8d0da5e9056c156394c17cb7be158
Nelson Avellaneda <nelsonavellanedasanabria@gmail.com>
2026-04-22 20:41:08 -0500

    Cambio y reparacion de layout y logos

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,19 +15,19 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/My Icon.png',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/My Icon.png',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
+        url: '/My Icon.png',
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '/My Icon.png',
   },
 }
 
